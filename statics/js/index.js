39e001fd8b45866609662a168bede6503fb32302
Alberto Paparelli <alberto@paparelli.com.ar>
2021-05-10 22:45:45 -0300

    test revert

--- statics/js/index.js
+++ statics/js/index.js
@@ -31,7 +31,7 @@ function getCustomParams(params) {
 }
 
 function cargarProductos(productos) {
-  sessionStorage.setItem("productos", JSON.stringify(productos));
+  localStorage.setItem("productos", JSON.stringify(productos));
 }
 
 function crearProduct(product) {
@@ -54,14 +54,14 @@ function crearProduct(product) {
 }
 
 function irDetalle(idProducto) {
-  let productos = JSON.parse(sessionStorage.getItem("productos"));
+  let productos = JSON.parse(localStorage.getItem("productos"));
   const producto = productos.filter((p) => p.ID === idProducto)[0];
-  sessionStorage.setItem("producto", JSON.stringify(producto));
+  localStorage.setItem("producto", JSON.stringify(producto));
   location.href = location.origin + "/product.html";
 }
 
 function cargarProductoDetalle() {
-  const producto = JSON.parse(sessionStorage.getItem("producto"));
+  const producto = JSON.parse(localStorage.getItem("producto"));
   console.log(producto);
   let categorias = [];
   producto.categories.forEach((category) => categorias.push(category.name));
