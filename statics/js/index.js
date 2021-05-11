2b8367714938e5fd886fd4cfed4b6c1c82aac3c7
Alberto Paparelli <alberto@paparelli.com.ar>
2021-05-10 22:39:17 -0300

    fixes

--- statics/js/index.js
+++ statics/js/index.js
@@ -35,17 +35,19 @@ function cargarProductos(productos) {
 }
 
 function crearProduct(product) {
-  $(`<div class='container-product flex-column space-between'>
+  $(`<div class='container-product flex-column space-between box-shadow align-center'>
   <div class='container-image-product'>
       <img class='product' src='${product.thumbnail}' alt='producto'>
   </div>
-  <h2 class='detail-product font-roboto'>Título del Libro: ${product.title}</h2>
-  <h2 class='detail-product font-roboto'>Autor: ${product.author}</h2>
-  <h2 class='detail-product font-roboto'>Precio:</h2>
-  <div class='view-more flex-row space-around'>
-      <h2 class='more-info'><a class='share' href='./share.html'>Compartir</a></h2>
+  <div class="detail-info-book flex-column space-between">
+    <h2 class='detail-product font-roboto text-blue'>Título del Libro: ${product.title}</h2>
+    <h2 class='detail-product font-roboto text-blue'>Autor: ${product.author}</h2>
+    <h2 class='detail-product font-roboto text-blue'>Precio:</h2>
+  </div>
+  <div class='view-more flex-row space-around align-center'>
+      <h2 class='more-info'><a class='share align-center text-blue' href='./share.html'>Compartir</a></h2>
       <span>|</span>
-      <h2 class='container-view'><button class='view' onclick='irDetalle("${product.ID}")'>Ver +</button></h2>
+      <h2 class='container-view'><button class='view align-center' onclick='irDetalle("${product.ID}")'>Ver +</button></h2>
   </div>
   <button class='btn-add font-roboto text-white'>Agregar al carrito</button>
 </div>`).appendTo(document.getElementById("section-product"));
@@ -64,18 +66,18 @@ function cargarProductoDetalle() {
   let categorias = [];
   producto.categories.forEach((category) => categorias.push(category.name));
   categorias = categorias.join(" - ");
-  $(`<div class="container-photo-book">
+  $(`<div class="container-photo-book align-center">
     <img class="photo-book" src=${producto.cover} alt="producto">
   </div>
   <div class="flex-column space-around detail-book">
-    <h2 class="info-product font-roboto">Título del Libro: ${producto.title}</h2>
-    <h2 class="info-product font-roboto">Autor: ${producto.author}</h2>
-    <h2 class="info-product font-roboto">Editorial: ${producto.publisher}</h2>
-    <h2 class="info-product font-roboto">Fecha de publicación: ${producto.publisher_date}</h2>
-    <h2 class="info-product font-roboto">Idioma: ${producto.language}</h2>
-    <h2 class="info-product font-roboto">Categoría: ${categorias}</h2>
-    <h2 class="info-product font-roboto">Reseña del libro:</h2>
-    <p class="info-product font-roboto">${producto.content}</p>
+    <h2 class="info-product font-roboto text-blue">Título del Libro: ${producto.title}</h2>
+    <h2 class="info-product font-roboto text-blue">Autor: ${producto.author}</h2>
+    <h2 class="info-product font-roboto text-blue">Editorial: ${producto.publisher}</h2>
+    <h2 class="info-product font-roboto text-blue">Fecha de publicación: ${producto.publisher_date}</h2>
+    <h2 class="info-product font-roboto text-blue">Idioma: ${producto.language}</h2>
+    <h2 class="info-product font-roboto text-blue">Categoría: ${categorias}</h2>
+    <h2 class="info-product font-roboto text-blue">Reseña del libro:</h2>
+    <p class="info-product font-roboto text-blue">${producto.content}</p>
     <button class="btn-add font-roboto text-white">Agregar al carrito</button>
   </div>`).appendTo(document.getElementById("section-product"));
 }
